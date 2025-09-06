5c4004b6bdfc94b90078177922346fa994b0cf9d
Alef <aleftorresbr@gmail.com>
2025-09-06 18:41:34 -0300

    Adicionando um readme no projeot

--- src/main/java/com/mentoria/back_end_mentoria/resumo/ResumoController.java
+++ src/main/java/com/mentoria/back_end_mentoria/resumo/ResumoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/perfil/resumos")
+@RequestMapping(value = "/resumos")
 public class ResumoController {
 
     @Autowired
